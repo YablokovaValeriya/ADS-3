2b3c58845f2cf89de4222a00f4e4af2d8537217f
YablokovaValeriya <99179279+YablokovaValeriya@users.noreply.github.com>
2022-03-13 21:40:11 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -6,14 +6,12 @@ int cbinsearch(int *arr, int size, int value) {
     int mid = (LBound + RBound) / 2;
     if (arr[mid] == value) {
       int a = 0, b = 0;
-      for (a = mid - 1; arr[a] == value && a >= LBound; a--);
-      for (b = mid + 1: arr[b] == value && b <= RBound; b++);
+      for (a = mid - 1; arr[a] == value && a >= LBound; a--) {}
+      for (b = mid + 1: arr[b] == value && b <= RBound; b++) {}
       return (b - 1) - a;
-    }
-    else if (arr[mid] > value) {
+    } else if (arr[mid] > value) {
       RBound = mid;
-    }
-    else {
+    } else {
       LBound = mid + 1;
     }
   }
