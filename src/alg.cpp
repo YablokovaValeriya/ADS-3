93c31a61980edd8a539fb2adfc3904bc20eb5c32
YablokovaValeriya <99179279+YablokovaValeriya@users.noreply.github.com>
2022-03-13 21:27:31 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,6 +1,21 @@
 // Copyright 2021 NNTU-CS
 
 int cbinsearch(int *arr, int size, int value) {
-  //  поместить сюда реализацию алгоритма
-  return 0; // если ничего не найдено
+  int LBound = 0; RBound = size - 1;
+  while (LBound < RBound) {
+    int mid = (LBound + RBound) / 2;
+    if (arr[mid] == value) {
+      int a = 0, b = 0;
+      for (a = mid - 1; arr[a] == value && a >= LBound; a--);
+      for (b = mid + 1: arr[b] == value && b <= RBound; b++);
+      return (b - 1) - a;
+    }
+    else if (arr[mid] > value) {
+      RBound = mid;
+    }
+    else {
+      LBound = mid + 1;
+    }
+  }
+  return 0;
 }
